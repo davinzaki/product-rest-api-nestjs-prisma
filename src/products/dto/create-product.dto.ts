b83922c53fa02e0dd94d2ab5a7ce18ceb6d90415
Davin Zaki Pradipa <davinzaki12@gmail.com>
2025-06-10 09:44:08 +0700

    feat: add get all service and product dto

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProductDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  price: number;
+
+  @ApiProperty()
+  stock: number;
+
+  @ApiProperty()
+  category: number;
+}
