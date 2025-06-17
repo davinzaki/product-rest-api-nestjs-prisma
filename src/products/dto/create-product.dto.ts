5c76cedd9f0b673fdf05d02e6071d6c15c85343c
Davin Zaki Pradipa <davinzaki12@gmail.com>
2025-06-17 15:23:13 +0700

    feat: add crud product and input validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,15 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString, MinLength } from 'class-validator';
 
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   name: string;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty()
   price: number;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty()
   stock: number;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty()
   categoryId: number;
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,11 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ProductEntity } from './entities/product.entity';
 
 @Controller('products')
@@ -31,20 +32,30 @@ export class ProductsController {
   }
 
   @Get(':id')
-  @ApiCreatedResponse({ type: ProductEntity })
-  findOne(@Param('id') id: string) {
+  @ApiOkResponse({ type: ProductEntity })
+  // async findOne(@Param('id', ParseIntPipe) id: number) {
+  //   const article = await this.productsService.findOne(+id);
+  //   if (!article) {
+  //     throw new NotFoundException(`Product with ${id} does not exist.`);
+  //   }
+  //   return article;
+  // }
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(+id);
   }
 
-  @ApiCreatedResponse({ type: ProductEntity })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  @ApiCreatedResponse({ type: ProductEntity })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
   @ApiCreatedResponse({ type: ProductEntity })
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.remove(+id);
   }
 }
